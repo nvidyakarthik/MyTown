2b70bdf819b0bad37ae4af768ec182ab4f273bf6
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-15 20:48:42 -0700

    added force:true to drop and recreate database everytime

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.set("view engine", "handlebars");
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
   app.listen(PORT, function () {
     console.log("App now listening at localhost:" + PORT);
   });
