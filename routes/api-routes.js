70c405f8b39fb7749a808a2d9c120ba88167688e
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-17 09:55:58 -0700

    added code to list all acitivities in main page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,9 +7,45 @@ module.exports=function(app){
             var categoryObject={
                 categoryList:categoryData
             };
-            console.log(categoryObject);
+            //console.log(categoryObject);
             res.render("index", categoryObject);
        });
+        
+    });
+    app.get("/api/listAllActivities", function(req, res) {
+        db.Posts.findAll({
+            include: [{
+            model:db.Categories,
+            where: 
+            { 
+              id: db.Sequelize.col('Posts.fk_categoryid') 
+             },
+           attributes: ["categoryName"]  
+           }]
+
+        }).then(function(activityData){
+            var activityObject={
+                activities:activityData
+            };
+            console.log(activityObject);
+            res.render("index", activityObject);
+       });
+        
+    });
+    app.get("/login", function(req, res) {
+        res.render("login",{});
+       
+        
+    });
+
+    app.get("/register", function(req, res) {
+        res.render("register",{});
+       
+        
+    });
+    app.get("/postevent", function(req, res) {
+        res.render("postevent",{});
+       
         
     });
 }
