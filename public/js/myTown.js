f84b8bea4b88210fd4486324e5d481dd43d2647c
elspetha5 <elspetha@gmail.com>
2018-05-17 21:19:45 -0700

    user post info grabbed

--- public/js/myTown.js
+++ public/js/myTown.js
@@ -1,25 +1,44 @@
-$(document).ready(function() {
-    
-    var placeName = $("#placeName").val().trim();
-    var street = $("#street").val().trim();
-    var city = $("#city").val().trim();
-    var state = $("#state").val().trim();
-    var zip = $("#zip").val().trim();
-    var siteLink = $("#siteLink").val().trim();
-    var picLink = $("#picLink").val().trim();
-    var description = $("#description").val().trim();
-    var tag = $("#tag").val();
-    var kid = $("#kidFriendly");
-    
-    $("#placePost").on("submit", function(event) {
+$(document).ready(function () {
+
+    $("#placePost").on("submit", function (event) {
         event.preventDefault();
 
-        if (!placeName || !city || !state || !picLink || !description || !tag)
-        
-        var newPost = {};
-        
+        var placeName = $("#placeName").val().trim();
+        var street = $("#street").val().trim();
+        var city = $("#city").val().trim();
+        var state = $("#state").val().trim();
+        var zip = $("#zip").val().trim();
+        var siteLink = $("#siteLink").val().trim();
+        var picLink = $("#picLink").val().trim();
+        var description = $("#description").val().trim();
+        var tag = $("#tag").val();
+        var kid = $("#kidFriendly").val();
+
+        var newPost = {
+            name: placeName,
+            street: street,
+            city: city,
+            state: state,
+            zip: zip,
+            siteLink: siteLink,
+            picLink: picLink,
+            description: description,
+            tag: tag,
+            kid: kid
+        };
+
+        if (!placeName || !city || !state || !picLink || !description || !tag) {
+            //$("#postModal").modal("show");
+            alert("One or more of the required fields is missing, please check your submission.")
+        } else {
+            $.post("/api/posts", newPost, function () {
+                alert("Thank you for submitting a post! Your post is now waiting for review before being published.")
+            });
+        };
 
     });
+
+    $("")
 
 
 });
