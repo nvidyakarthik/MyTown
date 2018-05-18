2cfd4ac04017b1744e9f3b87819123bfe2dc739e
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-18 09:41:35 -0700

    more packages for passport added

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var passport   = require('passport');
+var session    = require('express-session');
+var env = require('dotenv').load();
+
 
 var PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 var db = require("./models");
 
 var app = express();
+
+// For Passport
+ 
+app.use(session({ secret: 'mykeyboard cat',resave: true, saveUninitialized:true})); // session secret 
+app.use(passport.initialize()); 
+app.use(passport.session()); // persistent login sessions
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
