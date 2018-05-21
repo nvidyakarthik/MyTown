7716bf6a3076680f6af02cebec0fb93bb8e92c4f
nvidya.karthik <nvidya.karthik@gmail.com>
2018-05-21 07:33:36 -0700

    added new flash message holder to list

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.set("view engine", "handlebars");
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.error=req.flash('norec_msg')
   res.locals.user = req.user || null;
   next();
 });
