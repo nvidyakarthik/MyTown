31ae06a43121676e3502d8b9a75699c5f777af89
Eva Hernandez <Eva@Dylans-MacBook-Pro.local>
2018-05-22 00:47:04 -0700

    Google API set up was set

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,8 @@
 function displayMap() {
-	var place = $(this).attr("data-name");
-	var queryURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyB2lCcLg3Cj16demvVcakCTtQfbud5lf2Q&libraries=places&callback=initAutocomplete";
+	var geocoder;
+	var map;
+	var APIkey = "AIzaSyDEodj1ydbJGQfBZlLer37smCWKXOauY5g"
+	var queryURL = "https://maps.googleapis.com/maps/api/js?key=" + APIkey + "&libraries=places&callback=initAutocomplete"
 
 	//ajax call 
 	$.ajax({
@@ -8,14 +10,13 @@ function displayMap() {
 		method: "GET"
 	}).then(function(response) {
 
-		var dataArray = response.data;
+		var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
+        });
 
-		for (var i = 0; i< dataArray.length; i++){
-
-			var mapDIV = $("<div>");
-
-			mapDIV.addClass("map")
-		}
+		
 
 		
 });
